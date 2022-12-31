29c1fc0253c244fc160efdb6915709c1971fe7ce
Nadiela Ahmeti <nadiela616@gmail.com>
2022-12-31 19:53:44 +0100

    solve

--- my-app/src/components/Map.js
+++ my-app/src/components/Map.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { BiMap } from 'react-icons/bi';
+
 
 const Map = ({ trips }) => {   
 
